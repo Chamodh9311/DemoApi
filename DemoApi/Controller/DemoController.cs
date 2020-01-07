ac914e3663706d7615eac0be18d3fa5301943465
chamodh <chamodh93@live.com>
2020-01-07 14:19:16 +0530

    Add create, delete and update methods

--- DemoApi/Controller/DemoController.cs
+++ DemoApi/Controller/DemoController.cs
@@ -2,42 +2,39 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Web;
 using System.Web.Http;
+using DemoApi.Model;
 
 namespace DemoApi.Controller
 {
     public class DemoController : ApiController
     {
+        private readonly List<CustomerList> _customerList = new List<CustomerList>();
+
         [HttpGet]
-        [Route("Demo/GetCustomerList")]
-        public HttpResponseMessage GetCustomerList()
+        [Route("Demo/CreateCustomerList/{id = id}/{email = email}/{name = name}")]
+        public HttpResponseMessage CreateCustomerList(int id, string email, string name)
         {
             var httpRequest = HttpContext.Current.Request;
             var bearerToken = httpRequest.Headers["Authorization"].Split(' ')[1];
-
+            
             if (!ValidateBearerToken(bearerToken)) return UnauthorizedRequest();
-            var customer1 = new List<string>() {"ID : 0001", "Name : John  Doe", "Email : jhon@live.com"};
-            var customer2 = new List<string>() {"Name : 0002", "Name : Jane Dan", "Email : jane@google.com"};
+
+            _customerList.Add(new CustomerList{Id = id , Email = email , Name = name });
 
             return ControllerContext.Request
-                .CreateResponse(HttpStatusCode.OK, new {customer1, customer2});
+                .CreateResponse(HttpStatusCode.OK, new { _customerList });
 
-        }
-
-        private HttpResponseMessage UnauthorizedRequest()
-        {
-            var unauthorizedRequest = new List<string>() { "Houston we have a problem!" , " Unauthorized request! :(" };
-            return ControllerContext.Request
-                .CreateResponse(HttpStatusCode.Unauthorized , new { unauthorizedRequest });
         }
 
         [HttpPost]
-        [Route("Demo/PutFileUpload")]
-        public HttpResponseMessage PutFileUpload()
+        [Route("Demo/FileUpload")]
+        public HttpResponseMessage FileUpload()
         {
             try
             {
@@ -56,7 +53,7 @@ namespace DemoApi.Controller
                         docFiles.Add(filePath);
                     }
 
-                    return Request.CreateResponse(HttpStatusCode.Created, docFiles);
+                    return Request.CreateResponse(HttpStatusCode.Created, "Path : " + docFiles.FirstOrDefault());
                 }
 
                 else
@@ -73,8 +70,8 @@ namespace DemoApi.Controller
         }
 
         [HttpGet]
-        [Route("Demo/GetFileDownload/{fileName}")]
-        public HttpResponseMessage GetFileDownload(string fileName)
+        [Route("Demo/FileDownload/{fileName = fileName}")]
+        public HttpResponseMessage FileDownload(string fileName)
         {
             try
             {
@@ -89,16 +86,16 @@ namespace DemoApi.Controller
                     if (!File.Exists(filePath))
                     {
                         //Throw 404 (Not Found) exception if File not found.
-                        response.StatusCode = HttpStatusCode.NotFound;
-                        response.ReasonPhrase = $"File not found: {fileName} .";
-                        throw new HttpResponseException(response);
+                        var error = $"File not found: {fileName}.txt";
+                        return ControllerContext.Request
+                            .CreateResponse(HttpStatusCode.NotFound, new { error });
                     }
 
                     var bytes = File.ReadAllBytes(filePath);
                     response.Content = new ByteArrayContent(bytes);
                     response.Content.Headers.ContentLength = bytes.LongLength;
                     response.Content.Headers.ContentDisposition =
-                        new ContentDispositionHeaderValue("attachment") {FileName = fileName};
+                        new ContentDispositionHeaderValue("attachment") {FileName = fileName+".txt" };
                     response.Content.Headers.ContentType =
                         new MediaTypeHeaderValue(MimeMapping.GetMimeMapping(fileName));
 
@@ -118,11 +115,79 @@ namespace DemoApi.Controller
             }
         }
 
+        [HttpPut]
+        [Route("Demo/UpdateCustomer/{id = id}/{email = email}")]
+        public HttpResponseMessage UpdateCustomer(int id, string email)
+        {
+            var httpRequest = HttpContext.Current.Request;
+            var bearerToken = httpRequest.Headers["Authorization"].Split(' ')[1];
+
+            if (!ValidateBearerToken(bearerToken)) return UnauthorizedRequest();
+
+            _customerList.Add(new CustomerList { Id = 01, Email = "jhon@live.com", Name = "John Doe" });
+
+            switch (id)
+            {
+                case 1:
+
+                    var customer1 = _customerList.Select(n =>
+                    {
+                        if (n.Id == 1)
+                        {
+                            n.Email = email;
+                        }
+
+                        return n;
+                    }).First();
+
+                    return ControllerContext.Request
+                         .CreateResponse(HttpStatusCode.Created, new { customer1 });
+
+                default:
+                    const string error = "Oops! No matching customer ID found!";
+
+                    return ControllerContext.Request
+                        .CreateResponse(HttpStatusCode.NotFound, new { error });
+            }
+        }
+
+        [HttpDelete]
+        [Route("Demo/DeleteCustomer/{id = id}")]
+        public HttpResponseMessage DeleteCustomer(int id)
+        {
+            var httpRequest = HttpContext.Current.Request;
+            var bearerToken = httpRequest.Headers["Authorization"].Split(' ')[1];
+
+            if (!ValidateBearerToken(bearerToken)) return UnauthorizedRequest();
+
+            switch (id)
+            {
+                case 1:
+
+                    var response = "deleted Id : " + id;
+                    return ControllerContext.Request
+                        .CreateResponse(HttpStatusCode.OK, new { response });
+
+                default:
+                    const string error = "Oops! No matching customer ID found!";
+
+                    return ControllerContext.Request
+                        .CreateResponse(HttpStatusCode.NotFound, new { error });
+            }
+        }
+
         private static bool ValidateBearerToken(string bearerToken)
         {
             var bearerTokenApp = ConfigurationManager.AppSettings["BearerToken"];
 
             return bearerTokenApp == bearerToken;
+        }
+
+        private HttpResponseMessage UnauthorizedRequest()
+        {
+            var unauthorizedRequest = new List<string>() { "Houston we have a problem!", " Unauthorized request! :(" };
+            return ControllerContext.Request
+                .CreateResponse(HttpStatusCode.Unauthorized, new { unauthorizedRequest });
         }
     }
 }
---
+++ DemoApi/Model/CustomerList.cs
@@ -0,0 +1,9 @@
+﻿namespace DemoApi.Model
+{
+    public class CustomerList
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+    }
+}
